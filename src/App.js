8b37957571aefb7292651994f5093b1c863c0a17
Kachielite <40024356+Kachielite@users.noreply.github.com>
2022-09-28 00:08:17 +0100

    Initialize socketIo and added signedIn event listener

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
+import axios from "axios";
 import React from "react";
-import {useEffect} from "react";
-import openSocket from "socket.io-client"
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
 function App() {
+  const [userData, setUserData] = useState({});
+  const [username, setUsername] = useState()
+  const socket = io("http://localhost:8080", { transports: ["websocket"] });
 
-  useEffect(() =>{
-    openSocket('http://localhost:8080',{ transports: ["websocket"] })
-  })
+  const onChangeHandler = (event) => {
+    event.preventDefault();
+    if (event.target.name === "username") {
+      setUserData({ ...userData, username: event.target.value });
+    }
+
+    if (event.target.name === "password") {
+      setUserData({ ...userData, password: event.target.value });
+    }
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8080/api/v1/sign-in", userData)
+      .then((response) => {
+        console.log('sign in successful')
+
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  useEffect(() => {
+    socket.on('signedIn', data => {
+      setUsername(data.message)
+    })
+  }, [socket])
 
   return (
     <div className="App">
-      <p>This is a react app</p>
+      <h1>This is a react app {username ? username : null}</h1>
+      <form onSubmit={onSubmitHandler}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            onChange={(event) => onChangeHandler(event)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(event) => onChangeHandler(event)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
