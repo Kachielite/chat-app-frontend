97962acd15baebbc40eafb2071564abf924324d8
Kachielite <40024356+Kachielite@users.noreply.github.com>
2022-10-15 16:36:11 +0100

    option page complete

--- src/components/options.js
+++ src/components/options.js
@@ -1,6 +1,6 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
-import { useFilePicker } from 'use-file-picker';
+import { useFilePicker } from "use-file-picker";
 import ArrowBackIosSharpIcon from "@mui/icons-material/ArrowBackIosSharp";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
@@ -12,82 +12,25 @@ const Options = ({ width, setCloseOptions }) => {
   const IP = "192.168.1.153";
   const [passwordType, setPasswordType] = useState("password");
   const [confirmPasswordType, setConfirmPasswordType] = useState("password");
-  const [user, setUser] = useState({name:"", displayPicture:""});
-  const [updatedUserInfo, setUpdatedUserInfo] = useState({name:"", password:"", confirmPassword:"", file:""})
-  const inputFile = useRef(null);
-  const userId = localStorage.getItem("userId");
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
   const token = localStorage.getItem("token");
 
-  const [openFileSelector, { filesContent, loading, errors }] = useFilePicker({
-    readAs: 'DataURL',
-    accept: 'image/*',
-    multiple: true,
-    limitFilesConfig: { max: 1 },
-    // minFileSize: 0.1, // in megabytes
-    maxFileSize: 50,
-    imageSizeRestrictions: {
-      maxHeight: 900, // in pixels
-      maxWidth: 1600,
-      minHeight: 600,
-      minWidth: 768,
-    },
+  const [openFileSelector, { filesContent }] = useFilePicker({
+    readAs: "DataURL",
+    accept: "image/*",
   });
 
-
-
-
-  
-
-  const onChageInputHandler = (event) =>{
-    if(event.target.name === "name"){
-      setUpdatedUserInfo({...updatedUserInfo, name: event.target.value})
-    } 
-    if(event.target.name === "password"){
-      setUpdatedUserInfo({...updatedUserInfo, password: event.target.value})
-    } 
-    if(event.target.name === "confirmPassword"){
-      setUpdatedUserInfo({...updatedUserInfo, confirmPassword: event.target.value})
-    } 
-  }
-
-  
-
-  const updateProfileHandler = (event) =>{
-    event.preventDefault();
-    setUpdatedUserInfo({...updatedUserInfo, file: filesContent.content})
-    const config = {
-      headers: { Authorization: `Bearer ${token}` },
-    };
-    const data = updatedUserInfo
-    axios
-      .put(`http://${IP}:8080/api/v1/update-user-data`, 
-        data,
-        config,
-      )
-      .then((response) => {
-        console.log(response)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  
-  
-  useEffect(() => {
-    axios
-      .get(`http://${IP}:8080/api/v1/user/${userId}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        setUser({
-          name: response.data.name,
-          displayPicture: response.data.displayPicture,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [token, userId]);
+  const onChageInputHandler = (event) => {
+    if (event.target.name === "name") {
+      setUser({ ...user, name: event.target.value });
+    }
+    if (event.target.name === "password") {
+      setUser({ ...user, password: event.target.value });
+    }
+    if (event.target.name === "confirmPassword") {
+      setUser({ ...user, confirmPassword: event.target.value });
+    }
+  };
 
   const passwordTypeHandler = () => {
     if (passwordType === "text") {
@@ -103,6 +46,23 @@ const Options = ({ width, setCloseOptions }) => {
       setConfirmPasswordType("text");
     }
   };
+
+  const updateProfileHandler = (event) => {
+    event.preventDefault();
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const data = { ...user, profile_photo: filesContent[0]?.content };
+    axios
+      .put(`http://${IP}:8080/api/v1/update-user-data`, data, config)
+      .then((response) => {
+        localStorage.setItem("user", JSON.stringify(user));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="options_container">
       <div className="close_options" onClick={() => setCloseOptions(false)}>
@@ -115,19 +75,33 @@ const Options = ({ width, setCloseOptions }) => {
       </div>
 
       <div className="profile_photo_container">
-        {!user?.displayPicture ? 
-          <AccountCircleIcon
-            sx={{
-              fontSize: "300px",
-            }}
-          />
-          :
-          <img src={updatedUserInfo.file.length > 0 ? updatedUserInfo.file.content : user.displayPicture} alt={user.name}/>
-        }
+        <div className="profile_photo">
+          {filesContent[0]?.content ? (
+            <img
+              src={filesContent[0]?.content}
+              alt={user.username}
+              height={100}
+              width={100}
+            />
+          ) : user.profile_photo === "" ? (
+            <AccountCircleIcon
+              sx={{
+                fontSize: "300px",
+              }}
+            />
+          ) : (
+            <img
+              src={user.profile_photo}
+              alt={user.username}
+              height={40}
+              width={40}
+            />
+          )}
+        </div>
         <button className="edit_button" onClick={() => openFileSelector()}>
           <EditIcon
             sx={{
-              fontSize: width < 480 ? "40px" : "40px",
+              fontSize: width < 480 ? "30px" : "40px",
             }}
           />
         </button>
@@ -135,11 +109,21 @@ const Options = ({ width, setCloseOptions }) => {
 
       <form onSubmit={updateProfileHandler}>
         <div className="form_input">
-          <input type="text" name="name" placeholder="Name"  onChange={onChageInputHandler}/>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={onChageInputHandler}
+          />
           <AccountCircleIcon />
         </div>
         <div className="form_input">
-          <input type={passwordType} name="password" placeholder="Password" onChange={onChageInputHandler}/>
+          <input
+            type={passwordType}
+            name="password"
+            placeholder="Password"
+            onChange={onChageInputHandler}
+          />
           <div onClick={passwordTypeHandler}>
             {passwordType === "text" ? (
               <VisibilityOutlinedIcon />
